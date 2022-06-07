1a725d30c090537b092dc2667210755da03a10a1
Abel Bekele <abeltefera.com>
2022-06-07 14:07:58 -0700

    header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
+#include <unistd.h>
 #include "main.h"
 /**
- * * main - Entry point
- *
- * *
+ * * main - Check description
+ * 
+ * * Description: It prints the word _putchar, followed by a new line.
  *
  * * Return: Always 0 (Success)
  */
@@ -10,13 +11,11 @@
 int main(void)
 
 {
-	char *sh = "_putchar";
-	while (*sh)
-{
-	_putchar(*sh);
-
-	sh++;
-}
+	char word[8] = "_putchar";
+	
+	int i;
+	for (i=0; i < 8; i++)
+	_putchar(word[i]);
 	_putchar('\n');
 	return (0);
 }
