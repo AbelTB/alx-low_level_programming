d93d8a4f055a0891bda69fee429d75f6c3b630db
Abel Bekele <abeltefera.com>
2022-06-07 17:32:19 -0700

    Write a function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,21 +30,19 @@ void times_table(void)
 	d = (rone * cone);
 
 	if ((d / 10) > 0)
-
-{			
-
+{
 	_putchar((d / 10) + '0');
 
 }
 	else
 {
-
 	_putchar(' ');
 
 }
 	_putchar((d % 10) + '0');
 	
 	if (cone < 9)
+
 {
 	_putchar(',');
 
