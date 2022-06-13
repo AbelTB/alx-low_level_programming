22daeec61a9b33c3e02081683d49d57f94d16850
Abel Bekele <abeltefera.com>
2022-06-13 06:46:04 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
 /**
- * _strcat - a function that concatenates two strings
- * @dest: copy to
- * @src: copy from
- * Return: two concat strings
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: is a pointer int type
+ * Return: Always 0.
  */
-char *_strcat(char *dest, char *src)
+
+void reset_to_98(int *n)
 {
-	int i, n;
-
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
-
-	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
-	{
-	}
-
-	return (dest);
+*n = 98;
 }
