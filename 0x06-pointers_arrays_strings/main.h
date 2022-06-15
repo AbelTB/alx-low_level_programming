b38b43030dc1f21afacfa51c47b7d88dc5095819
Abel Bekele <abeltefera.com>
2022-06-15 16:36:26 -0700

    Header file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef main.h
+#define main.h
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
