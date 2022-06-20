c0f21cca58a744d74a10c2c6f9e917e9c78b536d
Abel Bekele <abeltefera.com>
2022-06-20 09:53:02 -0700

    Task 0.Write a function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef main_H
+#define main_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif
