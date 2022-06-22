a41af545ebe61ec942e5b53ae73cb491b4f3f8c4
Abel Bekele <abeltefera.com>
2022-06-22 04:54:53 -0700

    Task 0.Write a function that prints a string, followed by a new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef main_H
+#define main_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+
+
+#endif
