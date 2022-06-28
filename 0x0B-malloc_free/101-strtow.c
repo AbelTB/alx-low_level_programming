74b72aac36d362de851dde4c959b3c5b748ad604
Abel Bekele <abeltefera.com>
2022-06-28 14:05:07 -0700

    Task 6.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -45,15 +45,15 @@ void free_everything(char **string, int i)
 char **strtow(char *str)
 {
 	int total_words = 0, b = 0, c = 0, length = 0;
-	char **words, *found_word;
+	char **w, *fd_word;
 
 	if (str == 0 || *str == 0)
 		return (NULL);
 	total_words = number(str);
 	if (total_words == 0)
 		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
+	w = malloc((total_words + 1) * sizeof(char *));
+	if (w == 0)
 		return (NULL);
 	for (; *str != '\0' &&  b < total_words;)
 	{
@@ -61,31 +61,31 @@ char **strtow(char *str)
 			str++;
 		else
 		{
-			found_word = str;
+			fd_word = str;
 			for (; *str != ' ' && *str != '\0';)
 			{
 				length++;
 				str++;
 			}
-			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == 0)
+			w[b] = malloc((length + 1) * sizeof(char));
+			if (w[b] == 0)
 			{
 				free_everything(words, b);
 				return (NULL);
 			}
-			while (*found_word != ' ' && *found_word != '\0')
+			while (*fd_word != ' ' && *fd_word != '\0')
 			{
-				words[b][c] = *found_word;
-				found_word++;
+				w[b][c] = *fd_word;
+				fd_word++;
 				c++;
 			}
-				words[b][c] = '\0';
+				w[b][c] = '\0';
 				b++;
 				c = 0;
 				length = 0;
 				str++;
-			}
 		}
-		return (words);
+	}
+	return (words);
 }
 
