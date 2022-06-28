6a6754ad5897bbcae410721c414ed15675df5eec
Abel Bekele <abeltefera.com>
2022-06-28 14:25:14 -0700

    Task 6.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * number - function to calculate number of words
  * @str: string being passed to check for words
@@ -36,7 +35,6 @@ void free_everything(char **string, int i)
 		free(string[--i]);
 	free(string);
 }
-
 /**
  * strtow - function that splits string into words
  * @str: string being passed
@@ -86,5 +84,5 @@ char **strtow(char *str)
 			str++;
 		}
 	}
-	return (words);
+return (words);
 }
